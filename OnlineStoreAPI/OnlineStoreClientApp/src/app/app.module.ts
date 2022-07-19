4f73a03a3b5462364b2e1db46aaee78797d7fa4e
Dor Liberman <Dorlib318@gmail.com>
2022-07-19 14:39:55 +0300

    initial item list component

--- OnlineStoreAPI/OnlineStoreClientApp/src/app/app.module.ts
+++ OnlineStoreAPI/OnlineStoreClientApp/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemFormComponent } from './item-list/item-form/item-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    ItemFormComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
